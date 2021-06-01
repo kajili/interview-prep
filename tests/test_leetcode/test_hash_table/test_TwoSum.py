681e2d632bf9b231538195c656a7024b89893001
Kevin Ajili <kajili@ucsc.edu>
2021-05-31 21:11:18 -0700

    added unit testing for leetcode hash_table TwoSum problem

---
+++ tests/test_leetcode/test_hash_table/test_TwoSum.py
@@ -0,0 +1,14 @@
+import pytest
+
+from src.leetcode.hash_table.TwoSum import Solution
+
+tests_for_twoSum = [([2, 7, 11, 15], 9, [0, 1]),
+                    ([3, 2, 4], 6, [1, 2]),
+                    ([3, 3], 6, [0, 1])]
+
+
+@pytest.mark.parametrize("nums,target,expected", tests_for_twoSum)
+def test_checkPermutationSingleHashTable(nums, target, expected):
+    sol = Solution()
+    function_result = sol.twoSum(nums, target)
+    assert function_result == expected
