23184470527b92815f86796112cd7cde35abcc86
Kevin Ajili <kajili@ucsc.edu>
2021-05-31 22:13:52 -0700

    added dummy function for CTCI 1.3 URLify

--- src/cracking_the_coding_interview/ch_01_arrays_and_strings/Q1_03_URLify.py
+++ src/cracking_the_coding_interview/ch_01_arrays_and_strings/Q1_03_URLify.py
@@ -4,7 +4,10 @@
 # You may assume that the string has sufficient space at the end to hold the additional
 # characters,and that you are given the "true" length of the string.
 # (Note: If implementing in Java,please use a character array so that you can perform this operation in place.)
-#
+
+def URLify():
+    pass
+
 # EXAMPLE
-# Input: "Mr John Smith ", 13
+# Input: "Mr John Smith     ", 13
 # Output: "Mr%20John%20Smith"
