fdd14d720eb55156fc7a0484ed3482f516c9afec
Kevin Ajili <kajili@ucsc.edu>
2021-06-01 22:33:05 -0700

    completed URLify modifying string in place, through In python this requires converting the string to a new list which still took O(n) space, so I prefer using the     # above method which creates a new string since they both use extra space but that one is much cleaner. also included pytest logs to the repo to show working unit tests for all functions.

--- src/cracking_the_coding_interview/ch_01_arrays_and_strings/Q1_03_URLify.py
+++ src/cracking_the_coding_interview/ch_01_arrays_and_strings/Q1_03_URLify.py
@@ -21,8 +21,43 @@ def URLify_NewString(inputString, trueLength):
 
 
 def URLify_InPlace(inputString, trueLength):
-    pass
-    # return inputString
+    # Convert string to list to modify indices, convert back to string before returning at the end.
+    url = list(inputString)
+
+    # Count the number of spaces
+    spaceCount = 0
+    for charIndex in range(0, trueLength):
+        currentChar = url[charIndex]
+        if currentChar == ' ':
+            spaceCount += 1
+
+    # Create new index based on allocated number of spaces plus trueLength
+    # (which is trueLength + (spaceCount * 2) because the trueLength contains the original spaces as well,
+    # so we allocate two extra places for each '%20' symbol compared to the ' ' space).
+    newIndex = trueLength + (spaceCount * 2)
+
+    # Iterate backwards through the full string and if there is a space, replace the values for spaces with '%20'
+    # otherwise move any other character to the required index.
+    for charIndex in reversed(range(trueLength)):
+        if url[charIndex] == ' ':
+            url[newIndex - 3] = '%'
+            url[newIndex - 2] = '2'
+            url[newIndex - 1] = '0'
+            newIndex -= 3
+        else:
+            url[newIndex - 1] = url[charIndex]
+            newIndex -= 1
+
+    # Convert back to a string and strip trailing whitespace
+    # *reference for string strip(), rstrip(), and lstrip() functions:
+    #   https://www.journaldev.com/23625/python-trim-string-rstrip-lstrip-strip
+    url = "".join(url).rstrip()
+
+    return url
+
+
+if __name__ == '__main__':
+    print(URLify_InPlace('test string       ', 11))
 
 # EXAMPLE
 # Input: "Mr John Smith     ", 13
