0c6e699dc33167624751a44907acb0c8645d0043
Kevin Ajili <kajili@ucsc.edu>
2021-06-01 17:50:32 -0700

    completed CTCI 1.1 IsUnique problem in 4 ways, all passing unit testing

--- src/cracking_the_coding_interview/ch_01_arrays_and_strings/Q1_01_IsUnique.py
+++ src/cracking_the_coding_interview/ch_01_arrays_and_strings/Q1_01_IsUnique.py
@@ -3,8 +3,39 @@
 #  Implement an algorithm to determine if a string has all unique characters.
 #  What if you cannot use additional data structures?
 
-def isUnique(s):
-    pass
+def isUnique_SetLengthComparison(s):
+    stringSet = set(s)  # O(n) time and O(n) space
+
+    # Compare if the set is the same length as the original string.
+    # A set will only hold unique values so if they are not equal then at least one character repeated.
+    isUnique = len(s) == len(stringSet)  # O(1) time
+
+    return isUnique  # total O(n) time and O(n) space
+
+
+def isUnique_SetComparingEachLetter(s):
+    # Create a set and add each letter to it once.
+    # return False if that character has been seen in the set before.
+    stringSet = set()
+
+    for char in s:
+        if char in stringSet:
+            return False
+        stringSet.add(char)
+    return True
+
+
+def isUnique_HashTableComparingEachLetter(s):
+    # Create a hash table with keys being the letters of the string and values being their number of occurrences
+    # return False if value of any key goes above 1
+    stringHashTable = {}
+
+    for char in s:
+        if char in stringHashTable:
+            return False
+        stringHashTable[char] = 1
+    return True
+
 
 # If we can't use additional data structures, we can do the following:
 # 1. Compare every character of the string to every other character of the string.
@@ -14,3 +45,23 @@ def isUnique(s):
 #   Careful, though: many sorting algorithms take up extra space.
 
 # These solutions are not as optimal in some respects, but might be better depending on the constraints of the problem.
+
+def isUnique_ConstantSpace(s):
+    # Sort the string
+    sortedStr = sorted(s)
+    sortedStr = "".join(sortedStr)
+
+    # Find length of string
+    n = len(s)
+    print(sortedStr)
+    # Check if neighboring characters are identical
+    for i in range(1, n):
+        current = sortedStr[i]
+        previous = sortedStr[i - 1]
+        print(current,previous)
+        if (current == previous):
+            # if there are any values that are equal they will be next to each other after sorting
+            return False
+    # If we looped through and no values were equal we return True
+    return True
+
--- tests/test_cracking_the_coding_interview/test_ch_01_arrays_and_strings/test_Q1_01_IsUnique.py
+++ tests/test_cracking_the_coding_interview/test_ch_01_arrays_and_strings/test_Q1_01_IsUnique.py
@@ -1,6 +1,7 @@
 import pytest
 
-from src.cracking_the_coding_interview.ch_01_arrays_and_strings.Q1_01_IsUnique import isUnique
+from src.cracking_the_coding_interview.ch_01_arrays_and_strings.Q1_01_IsUnique import isUnique_SetLengthComparison, \
+    isUnique_SetComparingEachLetter, isUnique_HashTableComparingEachLetter, isUnique_ConstantSpace
 
 tests_for_isUnique = [("abcd",  # test_string
                        True),  # expected
@@ -12,10 +13,31 @@ tests_for_isUnique = [("abcd",  # test_string
                        False),
 
                       ("hb 627jh=j ()",
+                       False),
+
+                      ("hb 627j= ()",
                        False)]
 
 
 @pytest.mark.parametrize("test_string,expected", tests_for_isUnique)
-def test_isUnique(test_string, expected):
-    function_result = isUnique(test_string)
+def test_isUnique_SetLengthComparison(test_string, expected):
+    function_result = isUnique_SetLengthComparison(test_string)
+    assert function_result == expected
+
+
+@pytest.mark.parametrize("test_string,expected", tests_for_isUnique)
+def test_isUnique_SetComparingEachLetter(test_string, expected):
+    function_result = isUnique_SetComparingEachLetter(test_string)
+    assert function_result == expected
+
+
+@pytest.mark.parametrize("test_string,expected", tests_for_isUnique)
+def test_isUnique_HashTableComparingEachLetter(test_string, expected):
+    function_result = isUnique_HashTableComparingEachLetter(test_string)
+    assert function_result == expected
+
+
+@pytest.mark.parametrize("test_string,expected", tests_for_isUnique)
+def test_isUnique_ConstantSpace(test_string, expected):
+    function_result = isUnique_ConstantSpace(test_string)
     assert function_result == expected
