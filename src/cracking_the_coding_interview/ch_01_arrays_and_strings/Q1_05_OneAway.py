547f3250f8c0dda406ed2e15711852dc2c9fde8e
Kevin Ajili <kajili@ucsc.edu>
2021-06-02 22:58:56 -0700

    completed CTCI 1.5 oneAway function, passing unit tests

--- src/cracking_the_coding_interview/ch_01_arrays_and_strings/Q1_05_OneAway.py
+++ src/cracking_the_coding_interview/ch_01_arrays_and_strings/Q1_05_OneAway.py
@@ -11,4 +11,40 @@
 # pale, bake -> false
 
 def oneAway(str1, str2):
-    pass
+    lengthDifferenceOverOne = abs(len(str1) - len(str2)) > 1
+    if lengthDifferenceOverOne:
+        return False
+
+    # If strings are dif lengths, get the shorter and longer strings.
+    if len(str1) < len(str2):
+        shorter = str1
+        longer = str2
+    else:
+        shorter = str2
+        longer = str1
+
+    indexShorter = 0
+    indexLonger = 0
+    countDiff = 0
+
+    while (indexShorter < len(shorter) and indexLonger < len(longer)):
+
+        if shorter[indexShorter] != longer[indexLonger]:
+            # Return false if we found more than one difference
+            countDiff += 1
+            if countDiff > 1:
+                return False
+
+            if len(shorter) == len(longer):
+                # If the strings are not the same length then increment the longer one
+                indexLonger += 1
+            else:
+                # If the strings are the same length increment both pointers
+                indexShorter += 1
+                indexLonger += 1
+        else:
+            # If the characters are the same increment both pointers.
+            indexShorter += 1
+            indexLonger += 1
+
+    return True
